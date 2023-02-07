b8b7c2074f838a9e2565c0e5f2fb32e3d83a2035
AhmedAshraf0 <aa7225071@gmail.com>
2023-02-07 16:54:51 +0200

    adding-more-categories
    Took 1 hour 54 minutes

--- app/src/main/java/com/example/foodplanner/dashboard/view/HomeFragment.java
+++ app/src/main/java/com/example/foodplanner/dashboard/view/HomeFragment.java
@@ -7,8 +7,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.CompositePageTransformer;
-import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
@@ -22,8 +20,12 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
     private ViewPager2 viewPager2;
-    private RecyclerView recentRec;
+    private RecyclerView recentRec , category1Rec , category2Rec , country1Rec , country2Rec;
     private RecentViewAdapter recentViewAdapter;
+    private Category1Adapter category1Adapter;
+    private Category2Adapter category2Adapter;
+    private Country1Adapter country1Adapter;
+    private Country2Adapter country2Adapter;
     private List<SliderItem> images;
     private List<String> meals;
     private List<Integer> mealsPhotos;
@@ -48,9 +50,26 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         viewPager2 = view.findViewById(R.id.viewPager);
         recentRec = view.findViewById(R.id.recentRec);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
-        linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
-        recentRec.setLayoutManager(linearLayoutManager);
+        category1Rec = view.findViewById(R.id.category_one_rec);
+        category2Rec = view.findViewById(R.id.category_two_rec);
+        country1Rec=  view.findViewById(R.id.country_one_rec);
+        country2Rec = view.findViewById(R.id.country_two_rec);
+        LinearLayoutManager llMgrRecent , llMgrCat1 , llMgrCat2 , llMgrCount1 , llMgrCount2;
+        llMgrRecent= new LinearLayoutManager(view.getContext());
+        llMgrCat1 = new LinearLayoutManager(view.getContext());
+        llMgrCat2 = new LinearLayoutManager(view.getContext());
+        llMgrCount1 = new LinearLayoutManager(view.getContext());
+        llMgrCount2 = new LinearLayoutManager(view.getContext());
+        llMgrCat1.setOrientation(RecyclerView.HORIZONTAL);
+        llMgrCat2.setOrientation(RecyclerView.HORIZONTAL);
+        llMgrRecent.setOrientation(RecyclerView.HORIZONTAL);
+        llMgrCount1.setOrientation(RecyclerView.HORIZONTAL);
+        llMgrCount2.setOrientation(RecyclerView.HORIZONTAL);
+        recentRec.setLayoutManager(llMgrRecent);
+        category1Rec.setLayoutManager(llMgrCat1);
+        category2Rec.setLayoutManager(llMgrCat2);
+        country1Rec.setLayoutManager(llMgrCount1);
+        country2Rec.setLayoutManager(llMgrCount2);
 
         images = new ArrayList<>();
         meals =new ArrayList<>();
@@ -79,5 +98,13 @@ public class HomeFragment extends Fragment {
 
         recentViewAdapter = new RecentViewAdapter(meals,mealsPhotos);
         recentRec.setAdapter(recentViewAdapter);
+        category1Adapter = new Category1Adapter(meals,mealsPhotos);
+        category1Rec.setAdapter(category1Adapter);
+        category2Adapter = new Category2Adapter(meals,mealsPhotos);
+        category2Rec.setAdapter(category2Adapter);
+        country1Adapter = new Country1Adapter(meals,mealsPhotos);
+        country1Rec.setAdapter(country1Adapter);
+        country2Adapter = new Country2Adapter(meals,mealsPhotos);
+        country2Rec.setAdapter(country2Adapter);
     }
 }
