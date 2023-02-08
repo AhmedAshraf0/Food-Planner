7df5d072cafe46538ee7cc1d973a421982a2b6f3
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2023-02-08 10:42:02 -0800

    finish The Screen

---
+++ app/src/main/java/com/example/foodplanner/login_screen/view/LoginScreenController.java
@@ -0,0 +1,14 @@
+package com.example.foodplanner.login_screen.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.foodplanner.R;
+
+public class LoginScreenController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen_controller);
+    }
+}
