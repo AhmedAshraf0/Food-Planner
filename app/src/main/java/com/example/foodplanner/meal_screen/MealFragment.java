6c912df988d9e1832e6d077eba47a0c68b7a1ce0
AhmedAshraf0 <aa7225071@gmail.com>
2023-02-13 01:20:31 +0200

    simple-design
    Took 1 hour 34 minutes

---
+++ app/src/main/java/com/example/foodplanner/meal_screen/MealFragment.java
@@ -0,0 +1,30 @@
+package com.example.foodplanner.meal_screen;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodplanner.R;
+public class MealFragment extends Fragment {
+
+    public MealFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_meal, container, false);
+    }
+}
