dc8e27283207bc9fd3883c20a9161fbc2879e78f
AhmedAshraf0 <aa7225071@gmail.com>
2023-02-13 02:51:48 +0200

    adding-incgredients
    Took 1 hour 24 minutes

--- app/src/main/java/com/example/foodplanner/dashboard/view/RecentViewAdapter.java
+++ app/src/main/java/com/example/foodplanner/dashboard/view/RecentViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.foodplanner.dashboard.view;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,22 +10,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodplanner.R;
+import com.example.foodplanner.meal_screen.MealFragment;
 
 import java.util.List;
 
 public class RecentViewAdapter extends RecyclerView.Adapter<RecentViewAdapter.ViewHolder> {
     List<String> meals;
     List<Integer> mealsPhotos;
+    FragmentManager fragmentManager;
 
-    public RecentViewAdapter(List<String> meals, List<Integer> mealsPhotos) {
+    public RecentViewAdapter(List<String> meals, List<Integer> mealsPhotos , FragmentManager fragmentManager) {
         this.meals = meals;
         this.mealsPhotos = mealsPhotos;
+        this.fragmentManager = fragmentManager;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView cardView;
         private ImageView mealImage;
         private ImageButton favBtn;
         private TextView mealTitle;
@@ -35,6 +43,7 @@ public class RecentViewAdapter extends RecyclerView.Adapter<RecentViewAdapter.Vi
             favBtn = itemView.findViewById(R.id.buttonF);
             mealTitle = itemView.findViewById(R.id.mealTitle);
             addBtn = itemView.findViewById(R.id.addToScedule);
+            cardView = itemView.findViewById(R.id.mealCardView);
         }
 
         public ImageView getMealImage() {
@@ -52,6 +61,10 @@ public class RecentViewAdapter extends RecyclerView.Adapter<RecentViewAdapter.Vi
         public Button getAddBtn() {
             return addBtn;
         }
+
+        public CardView getCardView() {
+            return cardView;
+        }
     }
 
     @NonNull
@@ -68,6 +81,11 @@ public class RecentViewAdapter extends RecyclerView.Adapter<RecentViewAdapter.Vi
         holder.getMealImage().setImageResource(mealsPhotos.get(position));
         holder.getMealTitle().setText(meals.get(position));
         //actions on btns or whole card
+//        holder.getCardView().setOnClickListener(v -> {
+//            FragmentTransaction transaction = fragmentManager.beginTransaction();
+//            transaction.replace(R.id.main_activity,new MealFragment());
+//            transaction.commit();
+//        });
     }
 
     @Override
--- app/src/main/java/com/example/foodplanner/meal_screen/MealFragment.java
+++ app/src/main/java/com/example/foodplanner/meal_screen/MealFragment.java
@@ -2,15 +2,25 @@ package com.example.foodplanner.meal_screen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.foodplanner.R;
-public class MealFragment extends Fragment {
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MealFragment extends Fragment {
+    private RecyclerView ingredentsRec;
+    private IngredientsAdapter ingredientsAdapter;
+    private List<String> measuresTest , ingredentsTest;
     public MealFragment() {
         // Required empty public constructor
     }
@@ -26,5 +36,28 @@ public class MealFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_meal, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        measuresTest = new ArrayList<>();
+        ingredentsTest = new ArrayList<>();
+        ingredentsTest.add("Vegetable Oil");
+        ingredentsTest.add("Onion");
+        ingredentsTest.add("Garlic");
+        ingredentsTest.add("Ginger");
+        ingredentsTest.add("Coriander");
+        measuresTest.add("1 tbls");
+        measuresTest.add("1 finely chopped");
+        measuresTest.add("2 cloves chopped");
+        measuresTest.add("1 part");
+        measuresTest.add("1 Packet");
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
+        ingredentsRec = view.findViewById(R.id.ingredients_rec);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        ingredentsRec.setLayoutManager(linearLayoutManager);
+        ingredientsAdapter = new IngredientsAdapter(measuresTest,ingredentsTest);
+        ingredentsRec.setAdapter(ingredientsAdapter);
     }
 }
