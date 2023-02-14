f8c5d942364e5f633bf484b078986274f7df973c
AhmedAshraf0 <aa7225071@gmail.com>
2023-02-15 00:29:48 +0200

    adding-retrofit
    Took 1 hour 13 minutes

---
+++ app/src/main/java/com/example/foodplanner/network/models/FilterForMealModel.java
@@ -0,0 +1,29 @@
+package com.example.foodplanner.network.models;
+
+public class FilterForMealModel {
+    private String strMeal , strMealThumb , idMeal;
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public void setStrMeal(String strMeal) {
+        this.strMeal = strMeal;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public void setStrMealThumb(String strMealThumb) {
+        this.strMealThumb = strMealThumb;
+    }
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(String idMeal) {
+        this.idMeal = idMeal;
+    }
+}
